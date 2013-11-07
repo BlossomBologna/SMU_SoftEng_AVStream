bd1e21d85c6ec412aa62ac2189772f7a50a16d89
Chintan Patel <chintu890@gmail.com>
2013-11-06 20:49:49 -0600

    removed comments from AVMessenger.java

--- HorusClient/src/AVPackage/AVMessenger.java
+++ HorusClient/src/AVPackage/AVMessenger.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package AVPackage;
 
 import java.io.ByteArrayOutputStream;
 
-/**
- *
- * @author Windows 7
- */
 public class AVMessenger {
     
     ByteArrayOutputStream payload;
