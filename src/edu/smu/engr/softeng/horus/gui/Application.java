83095cff976761c717ae8da4c9a716d4ccff429b
Rowdy Howell <rowdyhowell@gmail.com>
2013-11-08 12:04:41 -0600

    Initial GUI classes put in project

---
+++ src/edu/smu/engr/softeng/horus/gui/Application.java
@@ -0,0 +1,46 @@
+package edu.smu.engr.softeng.horus.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+public class Application implements ActionListener {
+
+	//Panels
+	private LoginFrame loginFrame;
+	
+	/**
+	 * Starts the components and runs application
+	 */
+	public Application() {
+		//Create login frame, pass this as action listener
+		loginFrame = new LoginFrame(this);
+		loginFrame.pack();
+		loginFrame.setVisible(true);
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent action) {
+		//Process action pending action command
+		if(action.getActionCommand().equals(Constants.ACTION_LOGIN)) {
+			//Login Action
+			String desc = "Login info:\n";
+			desc += "Username: "+loginFrame.getUsername()+"\n";
+			desc += "Password: "+String.valueOf(loginFrame.getPassword());
+			Constants.showPopup(loginFrame, "Login Information", desc, JOptionPane.PLAIN_MESSAGE);
+		}
+	}
+	
+	/**
+	 * Runs the application
+	 * 
+	 * @param args - not needed.
+	 */
+	public static void main(String[] args) {
+		new Application();
+	}
+}
---
+++ src/edu/smu/engr/softeng/horus/gui/Constants.java
@@ -0,0 +1,23 @@
+package edu.smu.engr.softeng.horus.gui;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class Constants {
+	
+	//Actions for events
+	public static final String ACTION_LOGIN = "LOGIN";
+	
+	/**
+	 * Encapsulates the way of making message popups
+	 * 
+	 * @param parentFrame
+	 * @param title
+	 * @param descritpion
+	 * @param messageType
+	 */
+	public static void showPopup(JFrame parentFrame, String title, String descritpion, int messageType) {
+		//Default error message popup
+		JOptionPane.showMessageDialog(parentFrame, descritpion, title, messageType);
+	}
+}
