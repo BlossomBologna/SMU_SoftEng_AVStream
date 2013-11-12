c240c71c568c770f36d6c8ae4e1c01e2075f9003
Chintan Patel <chintu890@gmail.com>
2013-11-12 16:26:43 -0600

    Added controller and messenger skeletons to proj.
    Includes files AVController.java and AVMessenger.java

---
+++ src/AVMessenger.java
@@ -0,0 +1,18 @@
+import java.io.ByteArrayOutputStream;
+
+public class AVMessenger {
+    
+    ByteArrayOutputStream payload;
+    
+    public void setBuffer(ByteArrayOutputStream data)
+    {
+        payload = data;
+    }
+    
+    public ByteArrayOutputStream getBuffer()
+    {
+        return payload; 
+    }
+    
+    
+}
