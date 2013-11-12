beacfa0d127782cf20d5650b37e05d28851004ff
Joel George <joelgeorge@smu.edu>
2013-11-11 22:17:06 -0600

    external network calls to start capturing sound or stopping the capture

---
+++ soundcode/stopSound.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+import java.io.DataOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+
+public class stopSound  {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) throws Exception {
+     Socket socket1;
+    int portNumber = 1778;
+    String str = "stop";
+
+    socket1 = new Socket(InetAddress.getLocalHost(), portNumber);
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(socket1.getInputStream()));
+
+    PrintWriter pw = new PrintWriter(socket1.getOutputStream(), true);
+
+    pw.println(str);
+
+    while ((str = br.readLine()) != null) {
+      System.out.println(str);
+      pw.println("stop");
+      
+      
+    }
+
+    br.close();
+    pw.close();
+    socket1.close();
+    }
+}
