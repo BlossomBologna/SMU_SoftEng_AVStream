2f0716665586f8c2f718d27e09815b5c2e40e36d
Chintan Patel <chintu890@gmail.com>
2013-11-13 23:05:30 -0600

    Made all tabs to 6 spaces

--- src/edu/smu/engr/softeng/horus/av/AVController.java
+++ src/edu/smu/engr/softeng/horus/av/AVController.java
@@ -19,15 +19,15 @@ import com.googlecode.javacv.cpp.opencv_core.IplImage;
  */
 public class AVController {
     
-	private long secondsToRecord = 5;
-	
-	private OpenCVFrameGrabber grabber = null;
-	private Object micObject = null;
-	
-	private int imageWidth = 620;
-	private int imageHeight = 480;
-	private double frameRate = 24;
-	private int codec = com.googlecode.javacv.cpp.avcodec.AV_CODEC_ID_MPEG1VIDEO;
+      private long secondsToRecord = 5;
+      
+      private OpenCVFrameGrabber grabber = null;
+      private Object micObject = null;
+      
+      private int imageWidth = 620;
+      private int imageHeight = 480;
+      private double frameRate = 24;
+      private int codec = com.googlecode.javacv.cpp.avcodec.AV_CODEC_ID_MPEG1VIDEO;
 
     /**
      * Initializes video and audio
@@ -37,11 +37,11 @@ public class AVController {
      */
     public void execute(boolean recVideo, boolean recAudio) {
         if (recVideo) {
-        	initializeCamera();
+              initializeCamera();
         }
         
         if (recAudio) {
-        	initializeMic();
+              initializeMic();
         }
         
         startCapture();
@@ -62,82 +62,82 @@ public class AVController {
      */
     private void startCapture() {
 
-    	if (grabber != null) {
-    		try {
-				grabber.start();
-				
-				AVPackager avPackager;
-				
-				while (true) {
-					String fileName = String.valueOf(System.currentTimeMillis()) + ".mpeg";
-					File chunk = new File(fileName);
-								
-					FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(chunk, imageWidth, imageWidth, 2);
-					recorder.setVideoCodec(codec);
-					recorder.setFormat("mpeg");
-					recorder.setFrameRate(frameRate);
-					recorder.setVideoBitrate(5 * imageHeight * imageWidth);
-					
-					recorder.setAudioBitrate(64000);
-					recorder.setAudioChannels(2);
-					recorder.setAudioCodec(com.googlecode.javacv.cpp.avcodec.AV_CODEC_ID_AAC);
-					
-					IplImage grabbedImage = grabber.grab();
-					
-					recorder.start();
-					//got this for statement from http://stackoverflow.com/questions/2550536/java-loop-for-a-certain-duration
-					
-					for (long stop=System.currentTimeMillis() + secondsToRecord * (long)1000;stop>System.currentTimeMillis();){
-						grabbedImage = grabber.grab();
-						recorder.record(grabbedImage);
-						
-						/**
-						 * Audio team, the buffer that contains the audio sample should
-						 * be inserted in the line below. Replace 'Buffer...' with your
-						 * buffer object which contains your audio
-						 */
-						//recorder.record(Buffer...);
-					}
-					
-					recorder.stop();
-					
-					avPackager = new AVPackager(chunk);
-					avPackager.start();
-				}
-				
-				//grabber.stop();
-			} catch (Exception e) {
-				e.printStackTrace();
-			} catch (com.googlecode.javacv.FrameRecorder.Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-    	}
-		
-	}
+          if (grabber != null) {
+                try {
+                        grabber.start();
+                        
+                        AVPackager avPackager;
+                        
+                        while (true) {
+                              String fileName = String.valueOf(System.currentTimeMillis()) + ".mpeg";
+                              File chunk = new File(fileName);
+                                                
+                              FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(chunk, imageWidth, imageWidth, 2);
+                              recorder.setVideoCodec(codec);
+                              recorder.setFormat("mpeg");
+                              recorder.setFrameRate(frameRate);
+                              recorder.setVideoBitrate(5 * imageHeight * imageWidth);
+                              
+                              recorder.setAudioBitrate(64000);
+                              recorder.setAudioChannels(2);
+                              recorder.setAudioCodec(com.googlecode.javacv.cpp.avcodec.AV_CODEC_ID_AAC);
+                              
+                              IplImage grabbedImage = grabber.grab();
+                              
+                              recorder.start();
+                              //got this for statement from http://stackoverflow.com/questions/2550536/java-loop-for-a-certain-duration
+                              
+                              for (long stop=System.currentTimeMillis() + secondsToRecord * (long)1000;stop>System.currentTimeMillis();){
+                                    grabbedImage = grabber.grab();
+                                    recorder.record(grabbedImage);
+                                    
+                                    /**
+                                     * Audio team, the buffer that contains the audio sample should
+                                     * be inserted in the line below. Replace 'Buffer...' with your
+                                     * buffer object which contains your audio
+                                     */
+                                    //recorder.record(Buffer...);
+                              }
+                              
+                              recorder.stop();
+                              
+                              avPackager = new AVPackager(chunk);
+                              avPackager.start();
+                        }
+                        
+                        //grabber.stop();
+                  } catch (Exception e) {
+                        e.printStackTrace();
+                  } catch (com.googlecode.javacv.FrameRecorder.Exception e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                  }
+          }
+            
+      }
 
     /**
      * Determined by Audio team
      * @param
      * @return
      */
-	private void initializeMic() {
+      private void initializeMic() {
 
-    	//method for audio team to initialize the microphone
-		//Yea. That. 
-		
-	}
+          //method for audio team to initialize the microphone
+            //Yea. That. 
+            
+      }
 
-	/**
-	 * The "Grabber" interacts with video camera. Sets the scale for frames. 
-	 * @param No parameter values.
-	 * @return No return value.
-	 */
-	private void initializeCamera() {
-    	grabber = new OpenCVFrameGrabber(0);
-    	grabber.setFrameRate(frameRate);
-    	grabber.setImageWidth(imageWidth);
-    	grabber.setImageHeight(imageHeight);
-	}
+      /**
+       * The "Grabber" interacts with video camera. Sets the scale for frames. 
+       * @param No parameter values.
+       * @return No return value.
+       */
+      private void initializeCamera() {
+          grabber = new OpenCVFrameGrabber(0);
+          grabber.setFrameRate(frameRate);
+          grabber.setImageWidth(imageWidth);
+          grabber.setImageHeight(imageHeight);
+      }
 
 }
