9386cfe99ed509d9a600599baf7ecd847359b1d0
GScruggs <gscruggs@smu.edu>
2013-11-13 23:31:45 -0600

    Update Capture.java
    Modified to meet coding standards.

--- SMU_SoftEng_AVStream-staging/src/audiorecorder/Capture.java
+++ SMU_SoftEng_AVStream-staging/src/audiorecorder/Capture.java
@@ -28,97 +28,94 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 /**
  *
- * @author joel
+ * @author Joel George, Audio, 11/11/2013
  */
 public class Capture  {
 
-    protected boolean running;
-    ByteArrayOutputStream out;
+      protected boolean running;
+      ByteArrayOutputStream out;
 
     
-    public void captureAudio() {
-        try {
-            final AudioFormat format = getFormat();
-            DataLine.Info info = new DataLine.Info(
-                    TargetDataLine.class, format);
-            final TargetDataLine line = (TargetDataLine) AudioSystem.getLine(info);
-            line.open(format);
-            line.start();
-            Runnable runner = new Runnable() {
-                int bufferSize = (int) format.getSampleRate()
-                        * format.getFrameSize();
-                byte buffer[] = new byte[bufferSize];
+      public void captureAudio() {
+            try {
+                  final AudioFormat format = getFormat();
+                  DataLine.Info info = new DataLine.Info( TargetDataLine.class, format);
+                  final TargetDataLine line = (TargetDataLine) AudioSystem.getLine(info);
+                  line.open(format);
+                  line.start();
+                  Runnable runner = new Runnable() {
+                        int bufferSize = (int) format.getSampleRate() * format.getFrameSize();
+                        byte buffer[] = new byte[bufferSize];
 
-                public void run() {
-                    out = new ByteArrayOutputStream();
-                    running = true;
-                    try {
-                        while (running) {
-                            int count =
-                                    line.read(buffer, 0, buffer.length);
-                            if (count > 0) {
-                                out.write(buffer, 0, count);
-                            }
+                        public void run() {
+                              out = new ByteArrayOutputStream();
+                              running = true;
+                              try {
+                                    while (running) {
+                                          int count = line.read(buffer, 0, buffer.length);
+                                          if (count > 0) {
+                                                out.write(buffer, 0, count);
+                                          }
+                                    }     
+                                    out.close();
+                              } 
+                              catch (IOException e) {
+                                    System.err.println("I/O problems: " + e);
+                                    System.exit(-1);
+                              }
+                        }     
+                  };
+                  Thread captureThread = new Thread(runner);
+                  captureThread.start();
+            }      
+            catch (LineUnavailableException e) {
+                  System.err.println("Line unavailable: " + e);
+                  System.exit(-2);
+            }
+      }    
+
+      public void playAudio() {
+            try {
+                  byte audio[] = out.toByteArray();
+                  InputStream input = new ByteArrayInputStream(audio);
+                  final AudioFormat format = getFormat();
+                  final AudioInputStream ais = new AudioInputStream(input, format,
+                        audio.length / format.getFrameSize());
+                  DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+                  final SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+                  line.open(format);
+                  line.start();
+
+                  Runnable runner = new Runnable() {
+                        int bufferSize = (int) format.getSampleRate() * format.getFrameSize();
+                        byte buffer[] = new byte[bufferSize];
+
+                        public void run() {
+                              try {
+                                    int count;
+                                    while ((count = ais.read(buffer, 0, buffer.length)) != -1) {
+                                          if (count > 0) {
+                                                line.write(buffer, 0, count);
+                                          }
+                                    }
+                                    line.drain();
+                                    line.close();
+                              } 
+                              catch (IOException e) {
+                                    System.err.println("I/O problems: " + e);
+                                    System.exit(-3);
+                              }
                         }
-                        out.close();
-                    } catch (IOException e) {
-                        System.err.println("I/O problems: " + e);
-                        System.exit(-1);
-                    }
-                }
-            };
-            Thread captureThread = new Thread(runner);
-            captureThread.start();
-        } catch (LineUnavailableException e) {
-            System.err.println("Line unavailable: " + e);
-            System.exit(-2);
-        }
-    }
-
-    public void playAudio() {
-        try {
-            byte audio[] = out.toByteArray();
-            InputStream input =
-                    new ByteArrayInputStream(audio);
-            final AudioFormat format = getFormat();
-            final AudioInputStream ais =
-                    new AudioInputStream(input, format,
-                    audio.length / format.getFrameSize());
-            DataLine.Info info = new DataLine.Info(
-                    SourceDataLine.class, format);
-            final SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
-            line.open(format);
-            line.start();
-
-            Runnable runner = new Runnable() {
-                int bufferSize = (int) format.getSampleRate()
-                        * format.getFrameSize();
-                byte buffer[] = new byte[bufferSize];
-
-                public void run() {
-                    try {
-                        int count;
-                        while ((count = ais.read(
-                                buffer, 0, buffer.length)) != -1) {
-                            if (count > 0) {
-                                line.write(buffer, 0, count);
-                            }
-                        }
-                        line.drain();
-                        line.close();
-                    } catch (IOException e) {
-                        System.err.println("I/O problems: " + e);
-                        System.exit(-3);
-                    }
-                }
-            };
-            Thread playThread = new Thread(runner);
-            playThread.start();
-        } catch (LineUnavailableException e) {
-            System.err.println("Line unavailable: " + e);
-            System.exit(-4);
-        }
-    }
+                  };
+                
+                  Thread playThread = new Thread(runner);
+                  playThread.start();
+            } 
+            catch (LineUnavailableException e) {
+                  System.err.println("Line unavailable: " + e);
+                  System.exit(-4);
+            }
+      }     
    
     
  /*private void storeAudio() {
@@ -171,21 +168,13 @@ public class Capture  {
         }
     }*/
     
-    public AudioFormat getFormat() {
-        float sampleRate = 44100.0F;
-        //8000,11025,16000,22050,44100
-        int sampleSizeInBits = 16;
-        //8,16
-        int channels = 2;
-        //1,2
-        boolean signed = true;
-        //true,false
-        boolean bigEndian = false;
-        //true,false
-        return new AudioFormat(sampleRate,
-                sampleSizeInBits,
-                channels,
-                signed,
-                bigEndian);
-    }//end getAudioFormat
+      public AudioFormat getFormat() {
+            float sampleRate = 44100.0F; //8000,11025,16000,22050,44100
+            int sampleSizeInBits = 16; //8,16
+            int channels = 2; //1,2
+            boolean signed = true; //true,false
+            boolean bigEndian = false; //true,false
+        
+            return new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
+      }
 }
