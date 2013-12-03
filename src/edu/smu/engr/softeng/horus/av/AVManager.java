1bb56f76ffa7ab5466c4ac90a2cd7036bc1960d4
Shilts <alex_x_videogames@yahoo.com>
2013-12-03 17:09:25 -0600

    AVManager Polish
    Added new variables and fleshed out the processAVFile() function

--- src/edu/smu/engr/softeng/horus/av/AVManager.java
+++ src/edu/smu/engr/softeng/horus/av/AVManager.java
@@ -6,8 +6,6 @@ package edu.smu.engr.softeng.horus.av;
 
 import java.io.File;
 
-//imports
-
 /**
  * AVManager The AVManager takes the AV unpackaged from Common Infrastructure's
  * Messages and hands it to the GUI to be played, as well as receives the signal
@@ -17,20 +15,21 @@ import java.io.File;
  */
 public class AVManager {
 
-	private static boolean recording;
+	private static boolean record;
+	private static boolean fileEnd;
 	private static File avFile;
 	private static File fullAVFile;
 
 	public AVManager() {
-		recording = false;
+		record = false;
+		fileEnd = false;
 	}
 
 	/**
 	 * Receives the decrypted AV File from AVReceiver and passes it along to the
 	 * GUI to be played
 	 * 
-	 * @param file
-	 *            : The AV File to be sent off
+	 * @param file : The AV File to be sent off
 	 */
 	public static void receiveAVFile(File file) {
 		avFile = file;
@@ -38,25 +37,35 @@ public class AVManager {
 	}
 
 	/**
-	 * Based on the value of the 'recording' variable, the current avFile is either appended
-	 * to the master file currently being recorded or deleted.
+	 * Based on the value of the 'recording' variable, the current avFile is
+	 * either appended to the master file currently being recorded or deleted.
 	 * 
-	 * @param avFile
-	 * 				: the AV File to either be appended to fullAVFile or deleted
+	 * @param avFile : the AV File to either be appended to fullAVFile or deleted
 	 */
 	private static void processAVFile(File avFile) {
-		if (recording) {
-			if(fullAVFile.length() > 0) {
+		if (record) {
+			if (fullAVFile.length() > 0) {
 				// Concatenate avFile to fullAVFile
-			} else {
-				fullAVFile = avFile;
+				System.out.println("File successfully concatinated");
 			}
-			System.out.println("File successfully concatinated");
+
+			if (fileEnd) {
+				record = false;
+				// save fullAVFile to disk;
+				if (fullAVFile.delete()) {
+					System.out.println(fullAVFile.getName() + " deleted");
+					fileEnd = false;
+				} else {
+					System.out.println(fullAVFile.getName() + " could not be deleted");
+				}
+			}
+
 		} else {
 			if (avFile.delete()) {
 				System.out.println(avFile.getName() + " deleted");
 			} else {
-				System.out.println(avFile.getName() + " could not be deleted");
+				System.out.println(avFile.getName()
+						+ " could not be deleted");
 			}
 		}
 	}
@@ -66,19 +75,18 @@ public class AVManager {
 	 * record (2), the AV File is done playing (1), or the client wants to stop
 	 * recording (0)
 	 * 
-	 * @param signal
-	 *            : the number indicating the current recording state
+	 * @param signal : the number indicating the current recording state
 	 */
 	public static void receiveGUISignals(int signal) {
 		switch (signal) {
 		case 0:
-			recording = false;
+			fileEnd = true;
 			break;
 		case 1:
 			processAVFile(avFile);
 			break;
 		case 2:
-			recording = true;
+			record = true;
 			break;
 		}
 	}
